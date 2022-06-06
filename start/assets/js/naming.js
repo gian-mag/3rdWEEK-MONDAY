befd8744eee397c611ab4e2b8e0350a8c5cf1e7f
gian-mag <106075481+gian-mag@users.noreply.github.com>
2022-06-06 17:02:32 +0200

    Add files via upload

---
+++ start/assets/js/naming.js
@@ -0,0 +1,7 @@
+function petA() {
+    document.getElementById("A").innerHTML = "Cane";
+  }
+  function petB() {
+    document.getElementById("B").innerHTML = "Gatto";
+  }
+console.log("Maschio" + "Femmina allora topo")
